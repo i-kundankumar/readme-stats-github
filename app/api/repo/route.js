cda779b3b98297288d61781e45b68f67c13e0aef
Kundan Kumar <kundankumar09187@outlook.com>
2026-01-03 12:36:54 +0530

    refactor: resize to fit

--- app/api/repo/route.js
+++ app/api/repo/route.js
@@ -45,7 +45,7 @@ export async function GET(req) {
     const description = data.description || "No description provided";
 
     // Wrap description (max 2 lines)
-    const maxLen = 55;
+    const maxLen = 50;
     let descLines = [description];
     if (description.length > maxLen) {
         let splitIndex = description.lastIndexOf(" ", maxLen);
@@ -57,7 +57,7 @@ export async function GET(req) {
     }
 
     const svg = `
-<svg width="450" height="200" viewBox="0 0 450 200" xmlns="http://www.w3.org/2000/svg">
+<svg width="400" height="200" viewBox="0 0 400 200" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <linearGradient id="bg" x1="0" y1="0" x2="1" y2="1">
       <stop offset="0%" stop-color="#0d1117"/>
